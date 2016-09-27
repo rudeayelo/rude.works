e4d04ad6bf1689ae646e879bf31cd08f4565c837
Rude <rude.ayelo@xing.com>
2016-09-27 09:23:20 +0200

    Adds react-ga to handle analytics

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Analytics from 'react-ga'
 import 'styles'
 import Home from 'scenes/Home'
+
+Analytics.initialize('UA-264378-1', { debug: false })
+Analytics.set({ page: window.location.pathname })
+Analytics.pageview(window.location.pathname)
 
 ReactDOM.render(
   <Home/>,
