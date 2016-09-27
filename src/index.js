6de8da539abe8b7cbaff98ffd994b7a5515692fd
Rude <rude.ayelo@xing.com>
2016-09-27 09:36:04 +0200

    Tracks outbound links

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Analytics from 'react-ga'
+import ReactGA from 'react-ga'
 import 'styles'
 import Home from 'scenes/Home'
 
-Analytics.initialize('UA-264378-1', { debug: false })
-Analytics.set({ page: window.location.pathname })
-Analytics.pageview(window.location.pathname)
+ReactGA.initialize('UA-264378-1', { debug: false })
+ReactGA.set({ page: window.location.pathname })
+ReactGA.pageview(window.location.pathname)
 
 ReactDOM.render(
   <Home/>,
