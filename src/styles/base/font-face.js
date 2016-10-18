14ddae9e046e7670d321cb23ef123015db7bc6c8
Rude <rude.ayelo@xing.com>
2016-10-18 11:11:33 +0200

    Fixes woff2 loading

--- src/styles/base/font-face.js
+++ src/styles/base/font-face.js
@@ -1,10 +1,10 @@
 import { injectGlobal } from 'styled-components'
 import LightWoff from 'styles/fonts/SourceSansPro-Light.woff'
-import LightWoff2 from 'styles/fonts/SourceSansPro-Light.woff'
+import LightWoff2 from 'styles/fonts/SourceSansPro-Light.woff2'
 import RegularWoff from 'styles/fonts/SourceSansPro-Regular.woff'
-import RegularWoff2 from 'styles/fonts/SourceSansPro-Regular.woff'
+import RegularWoff2 from 'styles/fonts/SourceSansPro-Regular.woff2'
 import SemiboldWoff from 'styles/fonts/SourceSansPro-Semibold.woff'
-import SemiboldWoff2 from 'styles/fonts/SourceSansPro-Semibold.woff'
+import SemiboldWoff2 from 'styles/fonts/SourceSansPro-Semibold.woff2'
 
 const FontFaceName = 'Source Sans Pro'
 const FontFaceDefs = [
@@ -32,8 +32,8 @@ const defineFontFace = (FontFaceDefs) => (
   FontFaceDefs.map((FontFaceDef) => (`
     @font-face {
       font-family: ${FontFaceName};
-      src:  url(${FontFaceDef.woff}) format('woff'),
-            url(${FontFaceDef.woff2}) format('woff2');
+      src:  url(${FontFaceDef.woff2}) format('woff2'),
+            url(${FontFaceDef.woff}) format('woff');
       font-weight: ${FontFaceDef.weight};
       font-style: ${FontFaceDef.style};
     }
